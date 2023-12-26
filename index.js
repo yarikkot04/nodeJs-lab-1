b901acece72d9d0ea275de4e9cfb69858c763961
yarikkot04 <yarikkotenkoim13@gmail.com>
2023-12-27 01:04:48 +0200

    Created a simple http server

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const i = 15 - 2
+import http from 'http'
 
-console.log(i)
+const PORT = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    res.end(`<h1>Lab 1. Node JS</h1>`)
+  }
+})
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
+})
